6be45466e64b2981e3f64cbf510fe9f377baa396
ravi rawat <ravirawat0@outlook.com>
2023-02-16 13:41:57 +0530

    F: minor changes

---
+++ C#/Assignment2/AdvancedCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class AdvancedCalculator : Calculator
+    {
+
+        public double Power(int @base, int exponent)
+        {
+            var power = Math.Pow(@base, exponent);
+            base.SetResult(power);
+            return power;
+        }
+
+        public override double GetResult()
+        {
+            return base.GetResult() * Math.Pow(10, 6);
+        }
+    }
+}
---
+++ C#/Assignment2/Calculator.cs
@@ -0,0 +1,35 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+
+        private double _result;
+        public void SetResult(double result)
+        {
+            _result = result;
+        }
+
+        public double Add(int a, int b)
+        {
+            _result = a + b;
+            return _result;
+        }
+
+        public double Add(int a, int b, int c)
+        {
+            _result = a + b + c;
+            return _result;
+        }
+
+        public double Add(double a, double b)
+        {
+            _result = a + b;
+            return _result;
+        }
+
+        public virtual double GetResult()
+        {
+            return _result;
+        }
+    }
+}
