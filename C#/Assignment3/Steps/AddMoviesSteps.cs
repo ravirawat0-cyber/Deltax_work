7fdcdc460a74f82ff8b7c6ab39e6cfd767715c16
ravi rawat <ravirawat0@outlook.com>
2023-02-16 16:35:00 +0530

    F:Csharp advanced assignment for BDD Scenario review

--- C#/Assignment3/Steps/AddMoviesSteps.cs
+++ C#/Assignment3/Steps/AddMoviesSteps.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace CsharpAdvanced.Steps
 {
+     [Binding]
     class AddMoviesSteps
     {
+        [Given(@"the user wants to add a new movie")]
+        public void GivenTheUserWantsToAddANewMovie()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the user select ""(.*)"" option")]
+        public void WhenTheUserSelectOption(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"enters movie details \(name, year of release, plot\)")]
+        public void WhenEntersMovieDetailsNameYearOfReleasePlot()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"selects actors from the list")]
+        public void WhenSelectsActorsFromTheList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"selects a producer from the list")]
+        public void WhenSelectsAProducerFromTheList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the movie is added to the movie list")]
+        public void ThenTheMovieIsAddedToTheMovieList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
     }
 }
