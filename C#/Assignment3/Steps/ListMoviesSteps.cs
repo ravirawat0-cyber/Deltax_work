62e0392a84c6d9432b934b1ef907c8d0566cbdd1
ravi rawat <ravirawat0@outlook.com>
2023-02-16 16:36:43 +0530

    F:Csharp advanced assignment for BDD Scenario review

--- C#/Assignment3/Steps/ListMoviesSteps.cs
+++ C#/Assignment3/Steps/ListMoviesSteps.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace CsharpAdvanced.Steps
-{
+{    
+    [Binding]
     class ListMoviesSteps
     {
+        [Given(@"the user wants to view a list of all movies")]
+        public void GivenTheUserWantsToViewAListOfAllMovies()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the user selects the ""(.*)"" option")]
+        public void WhenTheUserSelectsTheOption(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"a list of all movies with their actors and producers is displayed")]
+        public void ThenAListOfAllMoviesWithTheirActorsAndProducersIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
     }
 }
